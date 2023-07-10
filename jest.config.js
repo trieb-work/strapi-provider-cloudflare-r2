01124f8128caad1547e44f2600f23962b4c4ea32
Tilman Marquart <tilman@trieb.work>
2023-07-10 14:38:25 +0200

    fix: config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-'use strict';
+"use strict";
 
-const baseConfig = require('../../../jest.base-config');
-const pkg = require('./package.json');
+const pkg = require("./package.json");
 
 module.exports = {
-  ...baseConfig,
   displayName: pkg.name,
   roots: [__dirname],
 };
